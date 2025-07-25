50d4af92f30e0f103d5305410ccc777888333fa2
sainyusuf <k4mbingoce@yahoo.com>
2025-07-25 16:34:37 +0800

    2nd Version.

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     image_version = os.getenv("IMAGE_VERSION", "unknown")
-    return "🚀🚀 Hello there, this is from Flask in with 1st version and under development!! 🚀🚀 Version: " + image_version
+    return "🚀🚀 Hello there, this is from Flask in with 2nd version and under development!! 🚀🚀 Version: " + image_version
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
